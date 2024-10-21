ef88e15020b59ebf15bac26113dee2784393d7fc
Shlomit Horn <shlomithorn@gmail.com>
2024-10-21 19:35:53 +0300

    Add draw-to-file func

---
+++ draw-to-file.js
@@ -0,0 +1,29 @@
+import fs from "fs"
+import { utilService } from "./util.service.js"
+
+drawSquareToFile()
+
+function drawSquareToFile() {
+    const str = getSquare(utilService.getRandomIntInclusive(3, 20))
+    writeToFile(str).then(() => {
+        setTimeout(drawSquareToFile, 200)
+    })
+}
+
+function getSquare(size) {
+    var str = "*".repeat(size) + "\n"
+    for (let i = 0; i < size; i++) {
+        str += "*" + " ".repeat(size - 2) + "*\n"
+    }
+    str += "*".repeat(size) + "\n"
+    return str
+}
+
+function writeToFile(str) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile("data/pic.txt", str, (err) => {
+            if (err) reject(err)
+            else resolve()
+        })
+    })
+}
