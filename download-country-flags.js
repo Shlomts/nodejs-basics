76a6f20c6b0f4cdf54e83e6f7ae6bb84c6cc54cb
Shlomit Horn <shlomithorn@gmail.com>
2024-10-22 08:03:58 +0300

    Make code pretty

--- download-country-flags.js
+++ download-country-flags.js
@@ -3,14 +3,15 @@ import { utilService } from "./util.service.js"
 downloadCountryFlags()
 
 function downloadCountryFlags() {
-    const countries = getCountries().then((countries) => {
+    getCountries().then((countries) => {
         console.log(
             "Countries:",
             countries.map((c) => c.name)
         )
-        downloadFlags(countries).then(() => {
-            console.log("Your flags are ready")
-        })
+        return downloadFlags(countries)
+    })
+    .then(() => {
+        console.log("Your flags are ready")
     })
 }
 
@@ -19,32 +20,22 @@ function downloadCountryFlags() {
 // return the top 5
 
 function getCountries() {
-    var countries = []
-
-    return new Promise((resolve, reject) => {
-        const raw = utilService
+        return utilService
             .readJsonFile("data/countries.json")
-            .then((rawData) => {
-                let sortData = rawData.sort((a, b) => b.population - a.population )
-                for (let i = 0; i < 5; i++) {
-                    countries.push(sortData[i])
-                }
-                resolve(countries)
-            })
-            .catch(reject)
-
-        return countries
-    })
+            .then((rawData) => 
+               rawData.sort((a, b) => b.population - a.population ).slice(0,5)
+            )
+            .catch((err) => console.log(err))
 }
 
 function downloadFlags(countries) {
     const prms = countries.map((country) => {
         return utilService.download(
-            country.flags.svg,
-            `flags/${country.name.common}.svg`
+            // country.flags.svg,
+            // `flags/${country.name.common}.svg`
 
-            // country.flags.png,
-            // `flags/${country.name.common}.png`
+            country.flags.png,
+            `flags/${country.name.common}.png`
         )
     })
     return Promise.all(prms)
